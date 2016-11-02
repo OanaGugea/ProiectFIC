a90b2c94e3ac7fe9472cb890cb24819bd3456f8c
Oana Gugea <oana_gugea@yahoo.com>
2016-11-02 13:29:58 +0200

    Save lab work

---
+++ video/CMakeFiles/videoDetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/videoDetector.dir/video.cpp.o"
+  "videoDetector.pdb"
+  "videoDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/videoDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
